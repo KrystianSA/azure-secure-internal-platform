fafc2415ae4eb5ed63a8d9468576319be04f3c9a
Krystian Sasiadek <ksasiadek64@gmail.com>
2026-07-07 19:27:51 +0200

    feat: configure remote state in Azure Blob Storage

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -1,9 +1,15 @@
 terraform {
-  backend "local" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "satfrestate"
+    container_name       = "tfstate-secure-internal-platform"
+    key                  = "terraform.tfstate"
+    use_azuread_auth     = true
+  }
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.58.0"
 
     }

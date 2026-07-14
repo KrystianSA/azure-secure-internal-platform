add41c939335d7fef83fda14cf94412192b17130
Krystian Sasiadek <ksasiadek64@gmail.com>
2026-07-14 16:46:56 +0200

    add a virtual network flow logs

--- Terraform/provision/monitoring.tf
+++ Terraform/provision/monitoring.tf
@@ -44,3 +44,26 @@ resource "azurerm_network_watcher" "network_watcher" {
   name                = "NetworkWatcher_polandcentral"
   resource_group_name = azurerm_resource_group.network_watcher_rg.name
 }
+
+resource "azurerm_network_watcher_flow_log" "virtual_network_flow_logs" {
+  network_watcher_name = azurerm_network_watcher.network_watcher.name
+  resource_group_name  = azurerm_resource_group.network_watcher_rg.name
+  name                 = "virtual_network_secure_internal_platform_flow_logs"
+
+  target_resource_id = azurerm_virtual_network.virtual_network.id
+  storage_account_id = azurerm_storage_account.storage_account_flow_logs.id
+  enabled            = true
+
+  retention_policy {
+    enabled = true
+    days    = 7
+  }
+
+  traffic_analytics {
+    enabled               = true
+    workspace_id          = azurerm_log_analytics_workspace.log_analytics_workspace.workspace_id
+    workspace_region      = azurerm_log_analytics_workspace.log_analytics_workspace.location
+    workspace_resource_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+    interval_in_minutes   = 10
+  }
+}

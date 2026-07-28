3281217c21ef5a4aa493e30414dbdbdbd367c26b
Krystian Sasiadek <ksasiadek64@gmail.com>
2026-07-28 17:33:43 +0200

    add policies : 1.not allowed creation of public ip AND 2.not allowed open traffic network from internet to nsg

--- Terraform/provision/data.tf
+++ Terraform/provision/data.tf
@@ -3,3 +3,12 @@ data "azurerm_client_config" "current" {}
 data "azuread_user" "Krystian" {
   object_id = var.krystian_object_id
 }
+
+data "azurerm_policy_definition_built_in" "not_allowed_resource_types" {
+  display_name = "Not allowed resource types"
+}
+
+data "azurerm_network_security_group" "nsg_id" {
+  name                = azurerm_network_security_group.vm_nsg.name
+  resource_group_name = module.rg_secure_internal_platform.resource_group_name
+}
--- Terraform/provision/policy.tf
+++ Terraform/provision/policy.tf
@@ -1,7 +1,3 @@
-data "azurerm_policy_definition_built_in" "not_allowed_resource_types" {
-  display_name = "Not allowed resource types"
-}
-
 resource "azurerm_resource_group_policy_assignment" "not_allowed_public_ip" {
   name                 = "not_allowed_public_ip"
   resource_group_id    = module.rg_secure_internal_platform.resource_group_id
@@ -22,4 +18,25 @@ PARAMS
   non_compliance_message {
     content = "Creation of public IP addresses is not allowed in this resource group."
   }
+}
+
+locals {
+  policy_data = jsondecode(file("${path.module}/policies-definitions/deny_open_ports_policy.json"))
+}
+
+resource "azurerm_policy_definition" "deny_open_ports_policy" {
+  name         = "deny-open-ports-policy"
+  policy_type  = "Custom"
+  mode         = local.policy_data.mode
+  display_name = local.policy_data.displayName
+  description  = local.policy_data.description
+  policy_rule  = jsonencode(local.policy_data.policyRule)
+  parameters   = jsonencode(local.policy_data.parameters)
+
+}
+
+resource "azurerm_resource_group_policy_assignment" "not_allowed_inbound_traffic_from_internet_on_nsg" {
+  name                 = "not_allowed_inbound_traffic_from_internet_on_nsg"
+  resource_group_id    = module.rg_secure_internal_platform.resource_group_id
+  policy_definition_id = azurerm_policy_definition.deny_open_ports_policy.id
 }
